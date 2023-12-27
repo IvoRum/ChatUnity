69f2852facd3ff93f33e80ec6340ac4f2941c694
IvoRum <ivailo.rumenov@outlook.com>
2023-12-27 17:03:05 +0200

    Altered Friend repository to return only user handle. User service for finding all friend is done. Needs pagening.

--- chat-server/chat-services-impl/src/com/tu/varna/chat/service/impl/UserServiceImpl.java
+++ chat-server/chat-services-impl/src/com/tu/varna/chat/service/impl/UserServiceImpl.java
@@ -1,18 +1,30 @@
 package com.tu.varna.chat.service.impl;
 
 import com.tu.varna.chat.common.dto.UserDto;
+import com.tu.varna.chat.common.dto.UserHandleDto;
+import com.tu.varna.chat.repository.FriendRepository;
 import com.tu.varna.chat.service.UserService;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 public class UserServiceImpl implements UserService {
+    private static FriendRepository friendRepository;
+
+    static {
+        friendRepository=new FriendRepository();
+    }
     @Override
     public void addFriend(int userSender, int userReceiver) {
         
     }
 
     @Override
-    public Set<UserDto> allFriends(int userId) {
-        return null;
+    public Set<UserHandleDto> allFriends(int userId) {
+        try {
+            return friendRepository.getFriendOfuser(userId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- chat-server/chat-services/src/com/tu/varna/chat/service/UserService.java
+++ chat-server/chat-services/src/com/tu/varna/chat/service/UserService.java
@@ -1,10 +1,11 @@
 package com.tu.varna.chat.service;
 
 import com.tu.varna.chat.common.dto.UserDto;
+import com.tu.varna.chat.common.dto.UserHandleDto;
 
 import java.util.Set;
 
 public interface UserService {
     void addFriend(int userSender,int userReceiver);
-    Set<UserDto> allFriends(int userId);
+    Set<UserHandleDto> allFriends(int userId);
 }
