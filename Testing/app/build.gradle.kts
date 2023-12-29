80515861719b1ca375cf79dca50180f372f38667
IvoRum <Ivailo.rumenov@outlook.com>
2023-12-29 17:07:18 +0200

    Removed empty android projects

--- Testing/app/build.gradle.kts
+++ Testing/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.chatnewdev"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.chatnewdev"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
