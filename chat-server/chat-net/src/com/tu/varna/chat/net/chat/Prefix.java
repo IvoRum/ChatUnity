2fd6a2c0710d4725ee89d2fd4627980c1c58230d
IvoRum <ivailo.rumenov@outlook.com>
2024-01-09 20:56:23 +0200

    Enum frgot

---
+++ chat-server/chat-net/src/com/tu/varna/chat/net/chat/Prefix.java
@@ -0,0 +1,27 @@
+package com.tu.varna.chat.net.chat;
+
+public enum Prefix {
+    SMS("sms"),
+    GMS("gms"),
+    GFR("gfr"),
+    FRI("frh");
+
+    private final String code;
+
+    Prefix(String code) {
+        this.code = code;
+    }
+
+    public String getNameCode() {
+        return code;
+    }
+
+    public static Prefix getInstance(String code) throws IllegalAccessException {
+        for (Prefix prefix : values()) {
+            if (prefix.getNameCode().equals(code)) {
+                return prefix;
+            }
+        }
+        throw new IllegalAccessException();
+    }
+}
