978c1f730b5df61555c7add4416bc0d9c524369b
IvoRum <ivailo.rumenov@outlook.com>
2024-01-12 22:57:09 +0200

    Created new log in clean for the interface.

--- chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/MainActivity.kt
+++ chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.example.chat_mobile_interface
 
 import android.Manifest
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnit.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
-import com.example.chat_mobile_interface.service.RunningService
 import com.example.chat_mobile_interface.ui.theme.ChatmobileinterfaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,20 +54,64 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             ChatmobileinterfaceTheme {
-                Button(onClick = {
-                    val intent = Intent(this, FriendList::class.java)
-                    /*
-                    Intent(applicationContext, RunningService::class.java).also {
-                        it.action = RunningService.Actions.START.toString()
-                        startService(it)
-                    }
-
-                     */
-                    startActivity(intent)
-                }) {
-                    Text(text = "Open friend list")
-                }
+                LogIn()
             }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun LogIn() {
+        var userName by rememberSaveable { mutableStateOf("") }
+        var password by rememberSaveable { mutableStateOf("") }
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = "Char Unity",
+                modifier = Modifier.padding(0.dp,60.dp),
+                color = Color(79, 81, 216, 100),
+                style = MaterialTheme.typography.bodyLarge,
+                fontFamily = FontFamily.Monospace,
+                fontSize= 56.sp
+            )
+            TextField(
+                value = userName,
+                onValueChange = { userName = it },
+                label = { Text("Enter email") }
+            )
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter password") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp)) {
+                Text(text = "Log in")
+
+            }
+        }
+    }
+
+    @Composable
+    fun NavigateToFriendList() {
+        Button(onClick = {
+            val intent = Intent(this, FriendList::class.java)
+            /*
+            Intent(applicationContext, RunningService::class.java).also {
+                it.action = RunningService.Actions.START.toString()
+                startService(it)
+            }
+
+             */
+            startActivity(intent)
+        }) {
+            Text(text = "Open friend list")
         }
     }
 }
