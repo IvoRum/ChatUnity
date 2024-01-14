4836ebac0e34ce8e93a9ac38c406b97ee1988efc
IvoRum <ivailo.rumenov@outlook.com>
2024-01-14 20:57:52 +0200

    mda

--- chat-server/chat-common/src/com/tu/varna/chat/common/dto/UserHandleDto.java
+++ chat-server/chat-common/src/com/tu/varna/chat/common/dto/UserHandleDto.java
@@ -1,4 +1,4 @@
 package com.tu.varna.chat.common.dto;
 
-public record UserHandleDto(int id, String firstName,String familyName) {
+public record UserHandleDto(int id, String firstName,String familyName, int conversation) {
 }
