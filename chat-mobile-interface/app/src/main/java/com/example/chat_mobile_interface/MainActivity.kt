c8cbf50f397a48751bd074ab23c182fe8630d5fb
IvoRum <ivailo.rumenov@outlook.com>
2024-01-14 11:52:14 +0200

    Added logo and cheking for the lag in view

--- chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/MainActivity.kt
+++ chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/MainActivity.kt
@@ -7,43 +7,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -74,9 +54,9 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 var userData = viewModel.logedDataFlow.collectAsState();
                 Scaffold(topBar = {
-                    TopAppBar(
+                    TopAppBar(modifier=Modifier.background(Color(80,120,230)),
                         title = {
-                            if (userData.value.id != 0) {
+                            if (userData.value.id != 0 && userData.value.id != 404) {
                                 Text(text = "${userData.value.id} Name:${userData.value.familyName}")
                             }
                         },
@@ -90,9 +70,12 @@ class MainActivity : ComponentActivity() {
                         },
                         actions = {
                             IconButton(onClick = { /* do something */ }) {
-                                Icon(
-                                    Icons.Rounded.AccountCircle, contentDescription = "Your Profile"
-                                )
+                                if (userData.value.id != 0 && userData.value.id != 404) {
+                                    Icon(
+                                        Icons.Rounded.AccountCircle,
+                                        contentDescription = "Your Profile"
+                                    )
+                                }
                             }
                         },
                     )
@@ -103,13 +86,13 @@ class MainActivity : ComponentActivity() {
                     ) {
                         NavHost(navController, startDestination = "login") {
                             composable("login") {
-                                LogIn(viewModel,navController)
+                                LogIn(viewModel, navController)
                             }
                             composable("home") {
                                 Home(navController, userData)
                             }
                             composable("chat/{userData}/{userName}") { backStackEntry ->
-                                Chat(viewModel,backStackEntry, userData)
+                                Chat(viewModel, backStackEntry, userData)
                             }
                         }
                     }
--- chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/composable/LogIn.kt
+++ chat-mobile-interface/app/src/main/java/com/example/chat_mobile_interface/composable/LogIn.kt
@@ -1,5 +1,8 @@
 package com.example.chat_mobile_interface.composable
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,36 +24,39 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.chat_mobile_interface.R
 import com.example.chat_mobile_interface.view.model.UserViewModel
 
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun LogIn(viewModel: UserViewModel,navController: NavHostController) {
+fun LogIn(viewModel: UserViewModel, navController: NavHostController) {
     var currentProgress by remember { mutableStateOf(0f) }
     var loading by remember { mutableStateOf(false) }
     var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    val isTextFieldEmpty by derivedStateOf {
+        userName.isBlank()
+    }
+    val isTextFieldEmptyPassword by derivedStateOf {
+        password.isBlank()
+    }
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .background(Color(80, 120, 230)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(
-            text = "Char Unity",
-            modifier = Modifier.padding(0.dp, 60.dp),
-            color = Color(79, 81, 216, 100),
-            style = MaterialTheme.typography.bodyLarge,
-            fontFamily = FontFamily.Monospace,
-            fontSize = 56.sp
-        )
+        Image(painter = painterResource(R.drawable.logo1), contentDescription = "logo")
         TextField(
             value = userName,
             onValueChange = { userName = it },
@@ -62,13 +69,18 @@ fun LogIn(viewModel: UserViewModel,navController: NavHostController) {
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
+        if (isTextFieldEmpty) {
+            Text("Please enter text", color = Color.Red)
+        }
         Button(
             onClick = {
-                loading = true
-                logInAction(viewModel,userName, password, navController) { progress ->
-                    currentProgress = progress
+                if (!isTextFieldEmpty && !isTextFieldEmptyPassword) {
+                    loading = true
+                    logInAction(viewModel, userName, password, navController) { progress ->
+                        currentProgress = progress
+                    }
+                    loading = false
                 }
-                loading = false
             },
             modifier = Modifier.padding(10.dp)
         )
@@ -79,7 +91,12 @@ fun LogIn(viewModel: UserViewModel,navController: NavHostController) {
         Button(
             onClick = {
                 loading = true
-                logInAction(viewModel,"deme@mail.com", "deme12345678", navController) { progress ->
+                logInAction(
+                    viewModel,
+                    "deme@mail.com",
+                    "deme12345678",
+                    navController
+                ) { progress ->
                     currentProgress = progress
                 }
                 loading = false
